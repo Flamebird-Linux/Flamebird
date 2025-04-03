a776b63585657115b4bf00e041057e07420545b3
flamebird-linux <xhaa123@163.com>
2025-04-03 15:11:47 +0800

    main：使用 clap 定义我们的主要入口点

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use clap::Command;
+
+fn cli_main() -> Command {
+    Command::new("flameforge").about("Next generation package manager")
+}
+
 fn main() {
-    println!("Hello, world!");
+    match cli_main().get_matches().subcommand() {
+        _ => println!("我们应该实现一个真正的 CLI，嗯？")
+    }
 }
